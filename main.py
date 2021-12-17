c0843bb355e414b00ab8f1872211ae15c7b7a4e2
Steinar Rennemo <rennemo@gmail.com>
2021-12-17 22:41:40 +0100

    Testing git, learning Python

---
+++ main.py
@@ -0,0 +1,18 @@
+# Goal: Learn stuff
+
+# datetime module?
+import datetime
+tid = datetime.datetime.now()
+
+print("Heisan!")
+
+fname = input("Fornavn: ")
+lname = input("Etternavn: ")
+bdate = input("Født (DD/MM/YY): ")
+
+print(fname + ", i dag er det", tid)
+# print(tid.strftime("%A"))
+# days since birth
+# tid = datetime.datetime(2020, 5, 17)
+# print(tid)
+
