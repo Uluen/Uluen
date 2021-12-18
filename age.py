ebe3d56721e409b6aae7d65c28857bf1e1a04c2c
Steinar Rennemo <rennemo@gmail.com>
2021-12-18 06:31:48 +0100

    Testing

---
+++ age.py
@@ -0,0 +1,18 @@
+# Goal: Learn stuff
+# ToDo: check input
+
+
+from datetime import date
+today = date.today()
+
+
+print("Hullo!")
+
+fname = input("Firstname: ")
+lname = input("Lastname: ")
+bdate = int(input("Born (YYYY): "))
+
+
+age = int(today.year - bdate)
+
+print(fname + ", you're", age, "years old!")
